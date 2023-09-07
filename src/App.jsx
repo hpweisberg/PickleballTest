b6b7479a28e8038b689c5e2c65d25c0e21872ed5
hpweisberg <hpweisberg@gmail.com>
2023-09-07 15:54:26 -0400

    add logo and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import TopTitle from './components/TopTitle/TopTitle'
 
 function App() {
 
   return (
     <>
+    <TopTitle />
       <p>test</p>
     </>
   )
---
+++ src/components/TopTitle/TopTitle.jsx
@@ -0,0 +1,10 @@
+const TopTitle = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <img src="/pickleballLogo.png" alt="All State Pickleball League logo" 
+      className="w-auto h-20"/>
+    </div>
+  );
+}
+
+export default TopTitle;
